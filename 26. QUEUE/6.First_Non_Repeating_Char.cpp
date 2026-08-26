7c43214fd7b4abfe08c628d5669568f7d29789c1
Mohammad Zaid Khan <zaidkhan24082006@gmail.com>
2026-08-27 00:04:54 +0530

    First Non Repeatting Char using Queue

---
+++ 26. QUEUE/6.First_Non_Repeating_Char.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    string s = "aabcxbx";
+    vector<int> freq(26);
+    int n = s.length();
+    queue<char> q1;
+
+    for(int i=0;i<n;i++) {
+        freq[s[i] - 'a']++;
+        q1.push(s[i]);
+
+        while(!q1.empty()  && freq[q1.front() - 'a'] > 1) {
+            q1.pop();
+        }
+
+        if(q1.empty()) {
+            cout << -1 << endl;
+        }
+        else {
+            cout << q1.front() << endl;
+        }
+    }
+
+    return 0;
+}
